1e43027f2c0e0c12f71d618630f63e9b1f0975df
chri <Manizzel@192.168.1.2>
2014-11-18 22:44:00 +0100

    3 types of markers are now available "coverage", "no coverage" and "mutation" description

--- org.epitest.runner/src/org/epitest/runner/ResultListener.java
+++ org.epitest.runner/src/org/epitest/runner/ResultListener.java
@@ -21,6 +21,7 @@ import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.Document;
 import org.pitest.mutationtest.ClassMutationResults;
+import org.pitest.mutationtest.DetectionStatus;
 import org.pitest.mutationtest.MutationResult;
 import org.pitest.mutationtest.MutationResultListener;
 import org.pitest.mutationtest.engine.MutationDetails;
@@ -79,12 +80,12 @@ final class ResultListener implements MutationResultListener {
 
 	private void printICompilationUnitInfo(ClassMutationResults results, IPackageFragment mypackage) throws CoreException {
 		for (ICompilationUnit unit : mypackage.getCompilationUnits()) {
-			
+
 			String fileName = results.getFileName();
 			String elementName = unit.getElementName();
-			
+
 			if (fileName.equals(elementName)) {
-				
+
 				createMarker(results, unit);
 			}
 		}
@@ -92,43 +93,71 @@ final class ResultListener implements MutationResultListener {
 
 	private void createMarker(ClassMutationResults results, ICompilationUnit unit) throws CoreException {
 		IResource resource = unit.getResource();
-		
+
 		Document document = new Document(unit.getSource());
 		for (MutationResult result : results.getMutations()) {
-			MutationDetails details = result.getDetails();
-			
-			
-			String statusDescription = details.getDescription();
-			int lineNumber = details.getLineNumber();
 
-			IMarker mutationMarker = resource.createMarker("org.epitest.mutationmarker");
-			mutationMarker.setAttribute(MESSAGE, statusDescription);
-			mutationMarker.setAttribute(LINE_NUMBER, lineNumber);
+
 			
-		
+			MutationDetails details = result.getDetails();
+
+			String mutationDdescription = details.getDescription();
+			int lineNumber = details.getLineNumber();
 			int offset = getLineOffset(document, lineNumber);
 			int lineLength = getLineLength(document, lineNumber);
-			IMarker coverageMarker = resource.createMarker("org.epitest.mutationmarker");
-			coverageMarker.setAttribute(CHAR_START, offset);
-			coverageMarker.setAttribute(CHAR_END, offset+lineLength);
+
+			DetectionStatus status = result.getStatus();
+			switch (status) {
+
+			case SURVIVED:
+				highlightMissingCoverage(resource, offset, lineLength);
+				createMutationDescription(resource, mutationDdescription, lineNumber);
+				break;
+			case NO_COVERAGE:
+				highlightMissingCoverage(resource, offset, lineLength);
+				break;
+			case KILLED:
+				highlightCoverage(resource, offset, lineLength);
+				break;
+			default:
+				break;
+			}
 
 		}
 
 	}
 
-	private int getLineLength(Document document, int lineNumber){
+	private void highlightCoverage(IResource resource, int offset, int lineLength) throws CoreException {
+		IMarker coverageMarker = resource.createMarker("org.epitest.coverage.yes");
+		coverageMarker.setAttribute(CHAR_START, offset);
+		coverageMarker.setAttribute(CHAR_END, offset + lineLength);
+	}
+
+	private void highlightMissingCoverage(IResource resource, int offset, int lineLength) throws CoreException {
+		IMarker coverageMarker = resource.createMarker("org.epitest.coverage.no");
+		coverageMarker.setAttribute(CHAR_START, offset);
+		coverageMarker.setAttribute(CHAR_END, offset + lineLength);
+	}
+
+	private void createMutationDescription(IResource resource, String statusDescription, int lineNumber) throws CoreException {
+		IMarker mutationMarker = resource.createMarker("org.epitest.mutationmarker");
+		mutationMarker.setAttribute(MESSAGE, statusDescription);
+		mutationMarker.setAttribute(LINE_NUMBER, lineNumber);
+	}
+
+	private int getLineLength(Document document, int lineNumber) {
 		try {
-			return document.getLineLength(lineNumber-1);
+			return document.getLineLength(lineNumber - 1);
 		} catch (BadLocationException e) {
-			throw new IllegalStateException(e.getMessage(),e);
+			throw new IllegalStateException(e.getMessage(), e);
 		}
 	}
 
 	private int getLineOffset(Document document, int lineNumber) {
 		try {
-			return document.getLineOffset(lineNumber-1);
+			return document.getLineOffset(lineNumber - 1);
 		} catch (BadLocationException e) {
-			throw new IllegalStateException(e.getMessage(),e);
+			throw new IllegalStateException(e.getMessage(), e);
 		}
 	}
 }
