758db7652c6a6fb5fed76d5401cbde07b5fa47f9
chri <Manizzel@192.168.1.2>
2014-11-22 20:20:57 +0100

    Bugfix: corrected the order of the mutation status -> Survived -> Killed -> NoCoverage

--- org.epitest.runner/src/org/epitest/runner/ResultListener.java
+++ org.epitest.runner/src/org/epitest/runner/ResultListener.java
@@ -1,10 +1,18 @@
 package org.epitest.runner;
 
+import static java.util.stream.Collectors.toList;
 import static org.eclipse.core.resources.IMarker.CHAR_END;
 import static org.eclipse.core.resources.IMarker.CHAR_START;
 import static org.eclipse.core.resources.IMarker.LINE_NUMBER;
 import static org.eclipse.core.resources.IMarker.MESSAGE;
 import static org.eclipse.jdt.core.IPackageFragmentRoot.K_SOURCE;
+import static org.pitest.mutationtest.DetectionStatus.KILLED;
+import static org.pitest.mutationtest.DetectionStatus.NO_COVERAGE;
+import static org.pitest.mutationtest.DetectionStatus.SURVIVED;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Predicate;
 
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IProject;
@@ -25,6 +33,9 @@ import org.pitest.mutationtest.DetectionStatus;
 import org.pitest.mutationtest.MutationResult;
 import org.pitest.mutationtest.MutationResultListener;
 import org.pitest.mutationtest.engine.MutationDetails;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ListMultimap;
 
 final class ResultListener implements MutationResultListener {
 
@@ -92,82 +103,117 @@ final class ResultListener implements MutationResultListener {
 	}
 
 	private void createMarker(ClassMutationResults results, ICompilationUnit unit) throws CoreException {
-		IResource resource = unit.getResource();
+		final SourceFile f = new SourceFile(unit);
+		ListMultimap<Integer, MutationResult> m = getLineMutations(results);
 
-		Document document = new Document(unit.getSource());
-		for (MutationResult result : results.getMutations()) {
+		for (Integer lineNumber : m.keys()) {
+			List<MutationResult> mutationResults = m.get(lineNumber);
+			if (markSurvived(f, mutationResults, lineNumber))
+				continue;
 
+			if (markNoCoverage(f, mutationResults, lineNumber))
+				continue;
 
-			
-			MutationDetails details = result.getDetails();
-			
-			String mutationDdescription = details.getDescription();
-			int lineNumber = details.getLineNumber();
-			
-			int offset = getLineOffset(document, lineNumber);
-			int length = getLineLength(document, lineNumber);
-			
-		
-			
-			DetectionStatus status = result.getStatus();
-			switch (status) {
+			markKilled(f, mutationResults, lineNumber);
 
-			case SURVIVED:
-				createMutationDescription(resource, mutationDdescription, lineNumber);
-				highlightMutation(resource, offset, length);
-				
-				break;
-			case NO_COVERAGE:
-				highlightMissingCoverage(resource, offset, length);
-				break;
-			case KILLED:
-				highlightCoverage(resource, offset, length);
-				break;
-			default:
-				break;
+		}
+
+	}
+
+	private ListMultimap<Integer, MutationResult> getLineMutations(ClassMutationResults results) {
+		Collection<MutationResult> mutations = results.getMutations();
+		ListMultimap<Integer, MutationResult> m = ArrayListMultimap.create(mutations.size(), 2);
+
+		mutations.forEach((MutationResult r) -> {
+			MutationDetails details = r.getDetails();
+			m.put(details.getLineNumber(), r);
+		});
+		return m;
+	}
+
+	private boolean markKilled(final SourceFile f, List<MutationResult> mutationResults, Integer lineNumber) throws CoreException {
+		List<MutationResult> lineMutations = filter(mutationResults, KILLED);
+		if (lineMutations.isEmpty())
+			return false;
+
+		f.createTextMarker("org.epitest.coverage.yes", lineNumber);
+
+		return true;
+	}
+
+	private boolean markNoCoverage(final SourceFile f, List<MutationResult> mutationResults, Integer lineNumber) throws CoreException {
+		List<MutationResult> lineMutations = filter(mutationResults, NO_COVERAGE);
+		if (lineMutations.isEmpty())
+			return false;
+
+		f.createTextMarker("org.epitest.coverage.no", lineNumber);
+
+		return true;
+	}
+
+	private boolean markSurvived(final SourceFile f, List<MutationResult> mutationResults, Integer lineNumber) throws CoreException {
+		List<MutationResult> lineMutations = filter(mutationResults, SURVIVED);
+		if (lineMutations.isEmpty())
+			return false;
+
+		for (MutationResult r : lineMutations) {
+			String mutationDescription = r.getDetails().getDescription();
+			f.createProblemMarker("org.epitest.mutationmarker", lineNumber, mutationDescription);
+		}
+		f.createTextMarker("org.epitest.mutationmarker", lineNumber);
+		return true;
+	}
+
+	private List<MutationResult> filter(List<MutationResult> m, DetectionStatus status) {
+		return m.stream().filter(statusIs(status)).collect(toList());
+	}
+
+	private Predicate<MutationResult> statusIs(DetectionStatus status) {
+		return (MutationResult r) -> r.getStatus() == status;
+	}
+
+	private static final class SourceFile {
+		private final IResource resource;
+		private final Document document;
+
+		SourceFile(ICompilationUnit unit) throws JavaModelException {
+			resource = unit.getResource();
+
+			document = new Document(unit.getSource());
+			document.getNumberOfLines();
+		}
+
+		public void createProblemMarker(String type, int lineNumber, String message) throws CoreException {
+			IMarker m = resource.createMarker(type);
+			m.setAttribute(MESSAGE, message);
+			m.setAttribute(LINE_NUMBER, lineNumber);
+		}
+
+		public void createTextMarker(String type, int lineNumber) throws CoreException {
+			IMarker m = resource.createMarker(type);
+
+			int offset = getLineOffset(lineNumber);
+			int length = getLineLength(lineNumber);
+			m.setAttribute(CHAR_START, offset);
+			m.setAttribute(CHAR_END, offset + length);
+		}
+
+		private int getLineLength(int lineNumber) {
+
+			try {
+				return document.getLineLength(lineNumber - 1);
+			} catch (BadLocationException e) {
+				throw new IllegalArgumentException(e.getMessage(), e);
 			}
-
 		}
 
-	}
-
-	private void highlightMutation(IResource resource, int offset, int lineLength) throws CoreException {
-		IMarker coverageMarker = resource.createMarker("org.epitest.mutationmarker");
-		coverageMarker.setAttribute(CHAR_START, offset);
-		coverageMarker.setAttribute(CHAR_END, offset + lineLength);
-	}
-	
-	private void highlightCoverage(IResource resource, int offset, int lineLength) throws CoreException {
-		IMarker coverageMarker = resource.createMarker("org.epitest.coverage.yes");
-		coverageMarker.setAttribute(CHAR_START, offset);
-		coverageMarker.setAttribute(CHAR_END, offset + lineLength);
-	}
-
-	private void highlightMissingCoverage(IResource resource, int offset, int lineLength) throws CoreException {
-		IMarker coverageMarker = resource.createMarker("org.epitest.coverage.no");
-		coverageMarker.setAttribute(CHAR_START, offset);
-		coverageMarker.setAttribute(CHAR_END, offset + lineLength);
-	}
-
-	private void createMutationDescription(IResource resource, String statusDescription, int lineNumber) throws CoreException {
-		IMarker mutationMarker = resource.createMarker("org.epitest.mutationmarker");
-		mutationMarker.setAttribute(MESSAGE, statusDescription);
-		mutationMarker.setAttribute(LINE_NUMBER, lineNumber);
-	}
-
-	private int getLineLength(Document document, int lineNumber) {
-		try {
-			return document.getLineLength(lineNumber - 1);
-		} catch (BadLocationException e) {
-			throw new IllegalStateException(e.getMessage(), e);
+		private int getLineOffset(int lineNumber) {
+			try {
+				return document.getLineOffset(lineNumber - 1);
+			} catch (BadLocationException e) {
+				throw new IllegalArgumentException(e.getMessage(), e);
+			}
 		}
 	}
 
-	private int getLineOffset(Document document, int lineNumber) {
-		try {
-			return document.getLineOffset(lineNumber - 1);
-		} catch (BadLocationException e) {
-			throw new IllegalStateException(e.getMessage(), e);
-		}
-	}
 }
