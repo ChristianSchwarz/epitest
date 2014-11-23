0cef7eef6405399eefd1e4525038db7e4889f201
chri <Manizzel@192.168.1.2>
2014-11-23 16:17:18 +0100

    Added JavaDoc

--- org.epitest.runner/src/org/epitest/report/Markers.java
+++ org.epitest.runner/src/org/epitest/report/Markers.java
@@ -1,11 +1,20 @@
 package org.epitest.report;
 
+/**
+ * Contains marker-ids that are used to highlight not-/covered and survived
+ * mutations. The marker ids are used by  marker extension points that are defined in the plugin.xml.
+ * 
+ * @author Christian Schwarz
+ *
+ */
 public class Markers {
+	/** Id of the marker that is used to highlight covered lines */
 	public static final String COVERAGE = "org.epitest.coverage.yes";
+	/** Id of the marker that is used to highlight uncovered lines */
 	public static final String NO_COVERAGE = "org.epitest.coverage.no";
+	/** Id of the marker that is used to highlight survived mutations and as problem marker */
 	public static final String SURVIVED = "org.epitest.mutationmarker";
-	
-	
-	
-	private Markers() {}
+
+	private Markers() {
+	}
 }
